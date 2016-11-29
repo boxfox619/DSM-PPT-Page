a1f3208699e97d369cbf1f3c0185bc4c33738b9b
river-stone-jin <sukjinjjang@gmail.com>
2016-11-29 09:17:31 +0900

    create snowman lol and page movement with keypad

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-$(document).ready(function () {
-var page = 1;
-page++;
- $("body").on('click', function(event) {
-      if (this.hash !== "") {
-        $('html, body').animate({
-          scrollTop: $('#'+page).offset().top
-      }, 500, function(){
-            console.log();
-        });
-      }
-      event.preventDefault();
+$(document).ready(function() {
+    var page = 1;
+
+    $("body").keypress(function(e) {
+        if (e.keyCode == 50) {
+            if (this.hash !== "") {
+                page++;
+                $('html, body').animate({
+                    scrollTop: $('#' + page).offset().top
+                }, 500, function() {
+                    console.log(page);
+                });
+            }
+            event.preventDefault();
+        } else if (e.keyCode == 56) {
+            if (page > 1) {
+                if (this.hash !== "") {
+                    page--;
+                    $('html, body').animate({
+                        scrollTop: $('#' + page).offset().top
+                    }, 500, function() {
+                        console.log('dbclick' + page);
+                    });
+                }
+                event.preventDefault();
+            }
+        }
     });
 });
